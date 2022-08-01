d638c981f732385b6ebcda00f406a84e1beda19d
Andrianna <andrianna.odell@gmail.com>
2022-08-01 12:02:14 -0400

    added Category by area

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Categories() {
 
-    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+    fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
     .then(resp => resp.json())
     .then(catData => console.log(catData))
 }
