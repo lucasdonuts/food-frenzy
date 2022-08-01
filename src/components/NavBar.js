4920e6531a3b160b504f7aff34676ac8c8d26c4f
Lucas Duncan <lucasdonuts@gmail.com>
2022-08-01 16:21:08 -0500

    Nav bar mostly finished

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,44 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
+import '../App.css';
 
 const NavBar = () => {
   return (
-    <header>
-      <Nav className="justify-content-start nav-bar">
-        <Link
-          to="/">
-          <h1>Food Frenzy</h1>
-        </Link>
-        {/* <div className="nav-links"> */}
-        <NavLink
-          to="/favorites"
-        >
-          <Button>
-            Favorites
-          </Button>
-        </NavLink>
-        <NavLink
-          to="/categories"
-        >
-          <Button>
-            Categories
-          </Button>
-        </NavLink>
-        {/* </div> */}
-      </Nav>
-    </header>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Nav>
+            <Nav.Item>
+              <LinkContainer to="/">
+                <Nav.Link>
+                  <Navbar.Brand>Food Frenzy</Navbar.Brand>
+                </Nav.Link>
+              </LinkContainer>
+            </Nav.Item>
+            <Nav.Item>
+              <LinkContainer exact to="/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+            </Nav.Item>
+            <Nav.Item>
+              <LinkContainer to="/categories">
+                <Nav.Link>Categories</Nav.Link>
+              </LinkContainer>
+            </Nav.Item>
+            <Nav.Item>
+              <LinkContainer to="/favorites">
+                <Nav.Link>Favorites</Nav.Link>
+              </LinkContainer>
+            </Nav.Item>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
